27929dfddb8d38440c3c72420ab8a5442f7e7115
Ed Langley <elangley@cj.com>
2017-05-20 23:30:11 -0700

    Cleanup index.js, add types to wiring

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import type {Unpacker} from './unpacker';
 import type {Request,Headers,Response} from './common-types';
 
 let {RequestUnpacker} = require('./unpacker');
-let {Dispatcher,OK} = require('./dispatcher');
+let {Dispatcher} = require('./dispatcher');
 
 function wiring() {
-  let requestUnpacker = RequestUnpacker();
-  let dispatcher = Dispatcher(requestUnpacker);
+  let requestUnpacker : Unpacker = RequestUnpacker();
+  let dispatcher : Dispatcher = Dispatcher(requestUnpacker);
   return (req : Request,res) => {
-    let [status, headers, content] = dispatcher.dispatch(req);
+    let [status, headers, content] : Response = dispatcher.dispatch(req);
     res.writeHead(status, headers);
     res.end(content);
   }
